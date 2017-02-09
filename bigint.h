3bbe77687a92085eb49b6545b556d08cdd484aaa
Katelyn <deprekate@gmail.com>
2017-02-08 16:21:54 -0800

    Added compare function

--- bigint.h
+++ bigint.h
@@ -1,18 +1,41 @@
+/* 
+
+    bigint
+
+    Copyright (C) 2017 Katelyn McNair and Robert Edwards
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
-int mod(int a, int b){
+#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
+static int mod(int a, int b){
         int c = a % b;
         return c < 0 ? c + b : c;
 }
 
-typedef struct{
+typedef struct bigint{
 	bool positive;
 	size_t size;
 	unsigned char data[];
 } bigint;
+extern const bigint BI_INF;
 
 static bigint* bi_new(size_t size){
 	bigint *b = calloc(sizeof(*b) + size, 1);
@@ -22,6 +45,13 @@ static bigint* bi_new(size_t size){
 	b->positive = true;
 	return b;
 }
+
+bigint* bi_inf(void){
+	size_t max_size = (size_t)-1;
+	bigint *a = bi_new(max_size);
+	return a;
+}
+
 
 static bigint* bi_from_string(const char *s){
 	bool positive = true;
@@ -123,28 +153,26 @@ bigint* bi_add(const bigint *a, const bigint *b){
 	return c;
 }
 bool bi_cmp(const bigint *a, const bigint *b){
+	bool val;
 	if(a->positive == b->positive){
-		if(a->positive){
-			if(a->size > b->size){
-				return true;
-			}else if(a->size < b->size){
-				return false;
-			}
+		if(a->size > b->size){
+			return a->positive;
+		}else if(a->size < b->size){
+			return !a->positive;
 		}else{
-			if(a->size > b->size){
-				return false;
-			}else if(a->size < b->size){
-				return true;
+			size_t max = (a->size > b->size ? a->size : b->size);
+			int i;
+			for(i=0; i<=max; ++i){
+				if(a->data[i] > b->data[i]){
+					return a->positive;
+				}else{
+					return false;
+				}
 			}
 		}
 	}else{
-		if(a->positive){
-			return true;
-		}else{
-			return false;
-		}
+		return a->positive;
 	}
-
 }
 
 
